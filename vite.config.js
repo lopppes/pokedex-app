056feb1fa961ef412c5f0658d9cd9378e70949a3
lopppes <sl263920@gmail.com>
2024-04-17 15:57:04 -0300

    config de deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+ // https://vite.js.dev/config/
 
 export default defineConfig({
+  base: "/pokedex-app",
   plugins: [react()]
 })
